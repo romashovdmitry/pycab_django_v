4c3643bd8fb161f3c019cabd94d233e672508e6c
Your Name <romashov.dmitry.py@gmail.com>
2023-05-31 22:14:31 +0400

    fix csrf

--- pycab/settings.py
+++ pycab/settings.py
@@ -39,7 +39,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['1507839-cy37741.tw1.ru', '*']
 
 
 # Application definition
